3a9445bd687c324184505ff4fb63ec10b53cc734
AKASH KUMAR <71625383+Akashk21@users.noreply.github.com>
2023-11-26 14:35:49 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import {AiFillLinkedin,AiOutlineTwitter} from 'react-icons/ai'
 const Footer= ()=> {
   return (
     <div className='footer-container'>
-        <p>2023 Shopping-Hopping All rights reserverd</p>
+        <p>@AkashKumar 2023 Shopping-Hopping All rights reserverd</p>
         <p className='icons'>
             <AiFillLinkedin/>
             <AiOutlineTwitter/> 
